dd0f152f5b31d11475236199392f9b143dedab40
23wangxiaoping <438510012@qq.com>
2021-03-28 14:17:28 +0800

    新增User类

---
+++ src/main/java/com/wyp/domain/User.java
@@ -0,0 +1,13 @@
+package com.wyp.domain;
+
+/**
+ * @program: gitTest
+ * @description:
+ * @author: WangYP
+ * @create: 2021-03-28 14:16
+ **/
+public class User {
+    private String name;
+    private Long id;
+
+}
