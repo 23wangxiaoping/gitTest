6061cf961df0cad23a32b02aab56e721616b12b1
23wangxiaoping <438510012@qq.com>
2021-03-28 14:23:32 +0800

    新增User控制器

---
+++ src/main/java/com/wyp/controller/Usercontroller.java
@@ -0,0 +1,19 @@
+package com.wyp.controller;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: gitTest
+ * @description:
+ * @author: WangYP
+ * @create: 2021-03-28 14:22
+ **/
+@Component
+public class Usercontroller {
+
+
+    public void getUserById()
+    {
+
+    }
+}
